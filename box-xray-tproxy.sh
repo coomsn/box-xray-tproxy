0abd3cb79347a87f6ab59d8719d695c5ebeb8427
coomsn <acstudycn@gmail.com>
2024-11-30 21:16:58 +0800

    Update box-xray-tproxy.sh

--- box-xray-tproxy.sh
+++ box-xray-tproxy.sh
@@ -1,3 +1,12 @@
 #!/system/bin/sh
+
 scripts_dir="/data/adb/box-xray-tproxy/scripts"
-(until [ "$(getprop sys.boot_completed)" -eq 1 ]; do sleep 5; done; "${scripts_dir}/start.sh") &
+(
+until [ "$(getprop sys.boot_completed)" -eq 1 ]; do
+    sleep 4
+done
+
+chmod 755 "${scripts_dir}/start.sh"
+
+"${scripts_dir}/start.sh" &
+)
